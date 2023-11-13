dae4fc97a04d67b2b601d455e39dfaf5ed20586f
Nathan Lima.G <nathanlg007@gmail.com>
2023-11-13 11:10:05 -0300

    adicionado o email de contatos

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando osistema de gerenciamento de jogos")
 console.log("apicaçao esta se conectando ao banco de dados")
+
+console. log("0 paulo fez esse commit em voce ver na aula!")
+console. log("Enviado para o GitHub")
